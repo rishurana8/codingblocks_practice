2b37c68adef3d54a537be1916c023e018005a502
rishurana8 <rishurana256@gmail.com>
2023-07-20 10:38:13 +0530

    added javascript file with changes

--- javascript_codingblocks/trial.js
+++ javascript_codingblocks/trial.js
@@ -1,41 +1,88 @@
-console.log("hello");
+// console.log("hello");
 
-function fruit(taste,color){
-     this.color = color;
-     this.taste = taste;
+// function fruit(taste,color){
+//      this.color = color;
+//      this.taste = taste;
+// }
+
+// let mango = new fruit("sweet","yellow");
+// let orange = new fruit("sour","orange");
+
+
+// class FruitClass{
+//     constructor(taste,color){
+//          this.color = color;
+//          this.taste = taste;
+//     }
+// };
+
+// let kiwi = new FruitClass("sour","green");
+
+
+
+
+// var bird ={
+//    x:100,
+//    y:200,
+//    color:"blue",
+//    eggs:[1,2,3,4],
+
+//    fly:function(){
+//       console.log("bird is flying",this.x,this.y);
+//    }
+
+
+
+// };
+
+// let FruitClass2 = class{
+//      constructor(taste,color){
+//         this.taste = taste;
+//         this.color = color;
+//      }
+// };
+
+
+// bird.eggs.forEach(function(idx,val){
+//     console.log(idx,val);
+// })
+
+
+// function outer(arg1){
+
+//     let var1 = 10;
+
+//     function inner(arg2){
+//         let var2 = 20;
+
+//         console.log(arg1,var1,arg2,var2);
+//     }
+
+//      return inner
+// }
+// let x = outer('param1');
+// x()
+
+// function checkThis(){
+//     console.log(this)
+// }
+
+// let name = 'John'
+
+// let g1 = 'Good Morning ' + name
+
+// let g2 = `Good Morning ${name}`
+
+let obj1 = {
+    a: 10,
+    b: 20,
+    c: 30
 }
 
-let mango = new fruit("sweet","yellow");
-let orange = new fruit("sour","orange");
-
-
-class FruitClass{
-    constructor(taste,color){
-         this.color = color;
-         this.taste = taste;
-    }
-}
-
-let kiwi = new FruitClass("sour","green");
+let obj2 = Object.create(obj1)
 
 
 
 
-var bird ={
-   x:100,
-   y:200,
-   color:"blue",
-   eggs:[1,2,3,4],
-
-   fly:function(){
-      console.log("bird is flying",this.x,this.y);
-   }
 
 
-
-};
-
-
-bird.eggs.forEach(function(idx,val){
-    console.log(idx,val);
-})
